2956c2a190c45288a63a982e25bdd82a695de395
Lorena Escobar <silvanae16@gmail.com>
2020-03-25 17:31:13 -0300

    remove submodule

---
+++ jaime5.8/resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>J'AIME</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js" =""></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="CSS/estilos.css">
+	<link href="https://fonts.googleapis.com/css?family=Amatic+SC&display=swap" rel="stylesheet">
+
+
+</head>
+<body>
+ <h1>Hola</h1>
+<?php
+/*
+//include "cabecera.html";
+
+include "header-banner.php";
+include "productosdestacados.php";
+include "footer.html";
+*/
+
+?>
+</body>
+</html>
