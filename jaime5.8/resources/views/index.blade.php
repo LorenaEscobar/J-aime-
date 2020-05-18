d491f11b7bcc883ee508e47d2a5afab9457f6537
sabrina <sabrinajaimee@gmail.com>
2020-05-18 12:21:43 -0300

    carrusel

--- jaime5.8/resources/views/index.blade.php
+++ jaime5.8/resources/views/index.blade.php
@@ -72,20 +72,31 @@
 </div>
 
 	<!-----------------MENU----------------------------->
-
 	<!-----------------BANNER----------------------------->
-	<div class="container">
-		<div class="row">
-			<div class="banner">
-					<div class="col-sm-12">
-						<a href="inicio"><img src="/Imagenes/banner.png"></a>
-					</div>
-			</div>
-		</div>
-	</div>
-	<!-----------------BANNER----------------------------->
+		<div class="container">
 
-
+	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="/Imagenes/banner.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/Imagenes/banner.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
 	<!-----------------NUEVOS MODELOS----------------------------->
 	<h1>Nuevos Modelos</h1>  <!-- lo cambiamos -->
  	<section class="catalogo">
